8338ebd56ac43c4e21c66936daff842199e008f2
ArtMoc <tem.mochalov@yandex.ru>
2022-01-19 13:07:58 +0300

    1

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
-
+#include <array>
+#include <vector>
 using namespace std;
+#define tab "\t"
+
+//#define STL_ARRAY
+#define STL_VECTOR
 
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef STL_ARRAY
+	array<int, 5> arr = { 3,5,8,13,21 };
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // STL_ARRAY
+
+
+#ifdef STL_VECTOR
+	vector<int> vec = { 0,1,1,2,3,5,8,13,21,34,55,89,144,232, };
+	cout << "Size:     " << vec.size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl; //есть только у вектора
+	cout << "MaxSize:  " << vec.max_size() << endl;
+	cout << sizeof(vec) << " byte" << endl;
+
+	vec.push_back(55);
+	vec.push_back(555);
+
+	for (int i = 0; i < vec.size(); i++)
+	{
+		//cout << vec[i] << tab;
+		cout << vec.at(i)  << tab;
+	}
+	cout << endl;
+
+	cout << "Size:     " << vec.size() << endl;
+	cout << "Capacity: " << vec.capacity() << endl; //есть только у вектора
+	cout << "MaxSize:  " << vec.max_size() << endl;
+	cout << sizeof(vec) << " byte" << endl;
+
+
+	/*vector<vector<int>> vec2;
+	cout << "Size:     " << vec2.size() << endl;
+	cout << "Capacity: " << vec2.capacity() << endl; //есть только у вектора
+	cout << "MaxSize:  " << vec2.max_size() << endl;
+	cout << sizeof(vec2) << " byte" << endl;*/
+#endif // STL_VECTOR
 
 }
