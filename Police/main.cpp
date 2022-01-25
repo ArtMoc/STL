fa27ba4522d386acb51f708b59d8ecea4586832a
ArtMoc <tem.mochalov@yandex.ru>
2022-01-25 21:37:52 +0300

    12545

--- Police/main.cpp
+++ Police/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <list>
 #include <Windows.h>
+#include<conio.h>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -95,15 +96,15 @@ void main()
 	//setlocale(LC_ALL, "");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	std::map<std::string, std::list<Crime>> base; /* =
+	std::map<std::string, std::list<Crime>> base;  /*=
 	{
 		std::pair<std::string, std::list<Crime>>("m777ko", std::list<Crime>{Crime(1, "улица Ленина"),Crime(2, "улица Октябрьская")}),
 		std::pair<std::string, std::list<Crime>>("b510ma", std::list<Crime>{Crime(3, "ТЦ Экватор")}),
 		std::pair<std::string, std::list<Crime>>("a213bb", std::list<Crime>{Crime(4, "Красная площадь")})
 	};*/
 
-	load(base);
-
+	//load(base);
+	
 	/*for (std::list<Crime>::iterator it = base["m777ko"].begin(); it != base["m777ko"].end(); ++it)
 	{
 		cout << *it << endl;
@@ -114,9 +115,38 @@ void main()
 	cout << "Введите номер автомобиля: "; cin >> licence_plate;
 	cout << "Введите правонарушение: "; cin >> crime;
 	base[licence_plate].push_back(crime);*/
+	//save(base);
+	//print(base);
 
-	print(base);
-	save(base);
+	int action;
+	do
+	{
+		system("CLS");
+		cout << "Выбирите действие: " << endl;
+		cout << "1. Загрузить базу: " << endl;
+		cout << "2. Сохранить базу: " << endl;
+		cout << "3. Вывести базу: " << endl;
+		cout << "4. Добавить запись: " << endl;
+		action = _getch();
+		cout << action << endl;
+		switch (action)
+		{
+		case '1': load(base); break;
+		case '2': save(base); break;
+		case '3': print(base); break;
+		case '4':
+		{
+			std::string licence_plate;
+			Crime crime;
+			cout << "Введите номер автомобиля: "; cin >> licence_plate;
+			cout << "Введите правонарушение: "; cin >> crime;
+			base[licence_plate].push_back(crime);
+		}
+		break;
+		}
+		//_getch();
+		system("PAUSE");
+	}while (action != 27);
 }
 
 void print(const std::map<std::string, std::list<Crime>>& base)
@@ -158,7 +188,7 @@ void load(std::map<std::string, std::list<Crime>>& base)
 		{
 			std::getline(fin, key, ':');
 			base[key];
-			std::getline(fin, value, ';');
+			std::getline(fin, value);
 			if (value.find(',') == std::string::npos)
 			{
 				base[key].push_back(Crime(std::stoi(value), value));
@@ -171,7 +201,7 @@ void load(std::map<std::string, std::list<Crime>>& base)
 				for (char* pch = strtok(all_crimes, ",;"); pch; pch = strtok(NULL, ",;"))
 				{
 					int crime_id = atoi(pch);
-					//pch++;
+					while(*pch == ' ')pch++;
 					base[key].push_back(Crime(crime_id, pch+1));
 				}
 				/*for (size_t start = -1, end = 0; value.find(',', start + 1) != std::string::npos; start = end)
