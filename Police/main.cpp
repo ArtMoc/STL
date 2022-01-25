22723b9a79ca2bb3af0b0b539e7431a741b1a316
ArtMoc <tem.mochalov@yandex.ru>
2022-01-25 21:01:22 +0300

    123

--- Police/main.cpp
+++ Police/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
 #include <list>
@@ -31,6 +32,10 @@ public:
 	{
 		return place;
 	}
+	std::string& get_place()
+	{
+		return place;
+	}
 	Crime(){}
 	Crime(int crime_id, const std::string& place)
 	{
@@ -45,9 +50,22 @@ public:
 	{
 		return os << crimes.at(crime_id) << ", " << place;
 	}
+	std::ofstream& print(std::ofstream& os)const
+	{
+		os << crime_id << " " << place;
+		return os;
+	}
 	std::istream& scan(std::istream& is)
 	{
 		is.ignore(); //»гнорирует 1 символ из буфера ввода(в данном случае "\n").
+		//std::getline(is, crime_id, ',');
+		cin >> crime_id;
+		std::getline(is, place);
+		return is;
+	}
+	std::ifstream& scan(std::ifstream& is)
+	{
+		//is.ignore(); //»гнорирует 1 символ из буфера ввода(в данном случае "\n").
 		//std::getline(is, crime_id, ',');
 		cin >> crime_id;
 		std::getline(is, place);
@@ -59,37 +77,46 @@ std::ostream& operator<<(std::ostream& os, const Crime& obj)
 {
 	return obj.print(os);
 }
+std::ofstream& operator<<(std::ofstream& os, const Crime& obj)
+{
+	return obj.print(os);
+}
 std::istream& operator>>(std::istream& is, Crime& obj)
 {
 	return obj.scan(is);
 }
 
 void print(const std::map<std::string, std::list<Crime>>& base);
+void save(const std::map<std::string, std::list<Crime>>& base);
+void load(std::map<std::string, std::list<Crime>>& base);
 
 void main()
 {
 	//setlocale(LC_ALL, "");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	std::map<std::string, std::list<Crime>> base =
+	std::map<std::string, std::list<Crime>> base; /* =
 	{
 		std::pair<std::string, std::list<Crime>>("m777ko", std::list<Crime>{Crime(1, "улица Ћенина"),Crime(2, "улица ќкт€брьска€")}),
 		std::pair<std::string, std::list<Crime>>("b510ma", std::list<Crime>{Crime(3, "“÷ Ёкватор")}),
 		std::pair<std::string, std::list<Crime>>("a213bb", std::list<Crime>{Crime(4, " расна€ площадь")})
-	};
+	};*/
+
+	load(base);
 
 	/*for (std::list<Crime>::iterator it = base["m777ko"].begin(); it != base["m777ko"].end(); ++it)
 	{
 		cout << *it << endl;
 	}*/
 
-	std::string licence_plate;
+	/*std::string licence_plate;
 	Crime crime;
 	cout << "¬ведите номер автомобил€: "; cin >> licence_plate;
 	cout << "¬ведите правонарушение: "; cin >> crime;
-	base[licence_plate].push_back(crime);
+	base[licence_plate].push_back(crime);*/
 
 	print(base);
+	save(base);
 }
 
 void print(const std::map<std::string, std::list<Crime>>& base)
@@ -101,5 +128,70 @@ void print(const std::map<std::string, std::list<Crime>>& base)
 		{
 			cout << *l_it << endl;
 		}
+	}
+
+}void save(const std::map<std::string, std::list<Crime>>& base)
+{
+	std::ofstream fout("base.txt");
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	{
+		fout << it->first << ":";
+		for (std::list<Crime>::const_iterator l_it = it->second.begin(); l_it != it->second.end(); ++l_it)
+		{
+			fout << *l_it << ", ";
+		}
+		fout.seekp(-2, std::ios_base::cur);//сдвигаем курсор на два символа влево относительно текущей позиции
+		fout << ";\n";
+	}
+	fout.close();
+	system("start notepad base.txt");
+}
+
+void load(std::map<std::string, std::list<Crime>>& base)
+{
+	std::ifstream fin("base.txt");
+	if (fin.is_open())
+	{
+		std::string key;
+		std::string value;
+		while (!fin.eof())
+		{
+			std::getline(fin, key, ':');
+			base[key];
+			std::getline(fin, value, ';');
+			if (value.find(',') == std::string::npos)
+			{
+				base[key].push_back(Crime(std::stoi(value), value));
+				base[key].back().get_place().erase(0, 1);
+			}
+			else
+			{
+				char* all_crimes = new char[value.size() + 1];
+				strcpy(all_crimes, value.c_str());
+				for (char* pch = strtok(all_crimes, ",;"); pch; pch = strtok(NULL, ",;"))
+				{
+					int crime_id = atoi(pch);
+					//pch++;
+					base[key].push_back(Crime(crime_id, pch+1));
+				}
+				/*for (size_t start = -1, end = 0; value.find(',', start + 1) != std::string::npos; start = end)
+				{
+					end = value.find(',', start + 1);
+					std::string buffer = value.substr(start + 1, end - start);
+					if (buffer.find_first_not_of(' ') != std::string::npos)
+						buffer.erase(0, buffer.find_first_not_of(' '));
+					int crime_id = std::stoi(buffer);
+					buffer.erase(0, 1);
+					base[key].push_back(Crime(crime_id, buffer));
+				}*/
+			}
+			
+		}
+		fin.close();
+		base.erase("");
+	}
+	else
+	{
+		std::cerr << "Error: file not found!" << endl;
 	}
 }
